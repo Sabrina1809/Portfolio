ff200463164afd605c235af6fde74c211cde7c4c
SABRINA FRITZ <SABRINA_FRITZ@GMX.NET>
2025-04-26 15:49:49 +0200

    changed logo, added translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,26 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { MeComponent } from './me/me.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, MeComponent, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
+   // NEU:
+   constructor(private translate: TranslateService) {
+    translate.addLangs(['de', 'en']);   // verfügbare Sprachen
+    translate.setDefaultLang('de');     // Standardsprache
+  }
+
+  // NEU:
+  switchLanguage(lang: string) {
+    this.translate.use(lang);
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core'; // <- TranslateModule importieren
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  currentLang: string = 'de';  // Setzt Deutsch als Voreinstellung
+  constructor(private translate: TranslateService) {
+    // Standard-Sprache einstellen
+    this.translate.setDefaultLang('de');
+    this.translate.use('de');
+  }
 
+  switchLanguage(lang: string) {
+    this.translate.use(lang);
+    this.currentLang = lang;  // Speichert die aktuell gewählte Sprache
+  }
 }
