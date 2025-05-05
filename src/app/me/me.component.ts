728fb6b6993e8d8bf78a8441a471ff6c0ba12277
SABRINA FRITZ <SABRINA_FRITZ@GMX.NET>
2025-05-05 09:13:13 +0200

    improved font-sizes and animation at me- and about component

--- src/app/me/me.component.ts
+++ src/app/me/me.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core'; // <<< hinzufügen!
+import { Component, OnInit } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-me',
   standalone: true,
-  imports: [TranslateModule], // <<< hier hinzufügen!
+  imports: [TranslateModule],
   templateUrl: './me.component.html',
   styleUrl: './me.component.scss'
 })
-export class MeComponent {}
+export class MeComponent implements OnInit {
+  showContent = false;
+  typedName = '';
+  fullName = 'Sabrina Fritz';
+  private typingInterval: any;
+
+  ngOnInit(): void {
+    this.showContent = true;
+    this.startTypewriter();
+  }
+
+  startTypewriter(): void {
+    this.typedName = '';
+    let index = 0;
+
+    clearInterval(this.typingInterval); // falls vorher einer lief
+    setTimeout(() => {
+      this.typingInterval = setInterval(() => {
+        if (index <= this.fullName.length) {
+          this.typedName = this.fullName.slice(0, index);
+          index++;
+        } else {
+          clearInterval(this.typingInterval);
+        }
+      }, 150);
+    }, 2750)
+ 
+  }
+}
