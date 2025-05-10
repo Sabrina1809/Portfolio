d7212eb91dd11e5b1a6481f6e0f86e68c6a4ec83
SABRINA FRITZ <SABRINA_FRITZ@GMX.NET>
2025-05-10 15:19:25 +0200

    user feedback contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -12,21 +13,61 @@ import { TranslateModule } from '@ngx-translate/core';
 })
 export class ContactComponent {
 
+  http = inject(HttpClient);
+
   contactData = {
     name: "",
     email: "",
     message: "",
   }
 
-  checkboxChecked: boolean = false;
+  
+  mailTest = true;
 
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    console.log('onsubmit erreicht');
+    
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            ngForm.resetForm();
+            console.log('Test');
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+     
+      console.log('mail gesendet');
+      this.showThankYou = true; // Zeige Nachricht
+      this.checkboxChecked = false; // Checkbox zurücksetzen
+      setTimeout(() => {
+        this.showThankYou = false; // Nach 5s wieder ausblenden
+        ngForm.resetForm();
+      }, 5000);
+    }
+  }
+
+  checkboxChecked: boolean = false;
+  showThankYou: boolean = false;
   // Toggle für Checkbox
   toggleCheckbox() {
     this.checkboxChecked = !this.checkboxChecked;
   }
 
-  onSubmit(ngForm: NgForm) {
-    if (ngForm.valid && ngForm.submitted)
-    console.log(this.contactData)
-  }
+
 }
