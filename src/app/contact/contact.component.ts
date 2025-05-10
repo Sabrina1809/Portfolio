9e1584a0e8e9f1ddc271b3691da7f27ea4158924
SABRINA FRITZ <SABRINA_FRITZ@GMX.NET>
2025-05-10 14:13:32 +0200

    added form validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,14 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [TranslateModule, CommonModule],
+  imports: [TranslateModule, CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  checkboxChecked: boolean = false;
+
+  // Toggle für Checkbox
+  toggleCheckbox() {
+    this.checkboxChecked = !this.checkboxChecked;
+  }
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted)
+    console.log(this.contactData)
+  }
 }
